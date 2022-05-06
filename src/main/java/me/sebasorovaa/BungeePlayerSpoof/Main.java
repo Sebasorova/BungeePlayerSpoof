58fdb37c3d6e657792df59eb9bef540ad35957ed
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2022-05-06 16:09:24 -0500

    BETA realistic count 0.4
    Added BETA realistic count and first advancements in config

--- src/main/java/me/sebasorovaa/BungeePlayerSpoof/Main.java
+++ src/main/java/me/sebasorovaa/BungeePlayerSpoof/Main.java
@@ -1,19 +1,60 @@
 package me.sebasorovaa.BungeePlayerSpoof;
 
+// all commented imports are just for the config file thing, dont mind them :)
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.api.plugin.Listener;
+// import java.io.InputStream;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+// import java.io.File;
+// import java.io.IOException;
+// import net.md_5.bungee.config.Configuration;
+// import net.md_5.bungee.config.ConfigurationProvider;
+// import net.md_5.bungee.config.YamlConfiguration;
+// import java.io.OutputStream;
+// import java.io.FileOutputStream;
+// import com.google.common.io.ByteStreams;
 
 public final class Main extends Plugin implements Listener {
     int fakepla = 14;
+    int changeit = 0;
     @Override
     public void onEnable() {
+        // This is a test for the config file, in development
+        //
+        // Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+        // ConfigurationProvider.getProvider(YamlConfiguration.class).save(config, new File(getDataFolder(), "config.yml"));
+        // if (!getDataFolder().exists()) {
+        //     getDataFolder().mkdir();
+        // }
+        // File configFile = new File(getDataFolder(), "config.yml");
+        // if (!configFile.exists()) {
+        //     try {
+        //         configFile.createNewFile();
+        //         try (InputStream is = getResourceAsStream("config.yml");
+        //              OutputStream os = new FileOutputStream(configFile)) {
+        //            ByteStreams.copy(is, os);
+        //         }
+        //     } catch (IOException e) {
+        //         throw new RuntimeException("Unable to create configuration file", e);
+        //     }
+        // }
+
         getLogger().info("PlayerSpoofer loaded");
         getProxy().getPluginManager().registerListener(this, this);
+    }
+
+    public void changeitfunc() {
+        Random changerandom = new Random();
+        int coinFlip2 = changerandom.nextInt(100);
+        if (coinFlip2 >= 50) {
+            changeit = 0;
+        } else {
+            changeit = 1;
+        }
     }
 
     public void countchange() {
@@ -36,13 +77,16 @@ public final class Main extends Plugin implements Listener {
     }
 
     @EventHandler
-    public void onPing(ProxyPingEvent event)
-    {
-            countchange();
-            ServerPing ping = event.getResponse();
-            ServerPing.Players current = ping.getPlayers();
-            ping.setPlayers( new ServerPing.Players( current.getMax(), fakepla, current.getSample() ) );
-            event.setResponse( ping );
+    public void onPing(ProxyPingEvent event) {
+            changeitfunc();
+                if (changeit == 0) {}
+                 else {
+                    countchange();
+                    ServerPing ping = event.getResponse();
+                    ServerPing.Players current = ping.getPlayers();
+                    ping.setPlayers(new ServerPing.Players(current.getMax(), fakepla, current.getSample()));
+                    event.setResponse(ping);
+                }
     }
 
     @Override
