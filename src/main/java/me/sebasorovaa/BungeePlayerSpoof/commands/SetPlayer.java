d3dde4a89bed361b54dba03ee946ce0c1c6445c5
BuildTools <sebastianrivera14@outlook.com>
2022-07-24 23:53:42 -0500

    v0.8.0 Pre-Release v2

--- src/main/java/me/sebasorovaa/BungeePlayerSpoof/commands/SetPlayer.java
+++ src/main/java/me/sebasorovaa/BungeePlayerSpoof/commands/SetPlayer.java
@@ -12,14 +12,13 @@ public class SetPlayer extends Command {
     public void execute(CommandSender sender, String[] args) {
         if (args.length == 0){
             sender.sendMessage(ChatColor.RED + "[BungeePlayerSpoof] " + ChatColor.RED + "Correct usage: /setplayers <number>");
-            ;
         } else {
             try {
                 fakepla = Integer.parseInt(args[0]);
                 sender.sendMessage(ChatColor.RED + "[BungeePlayerSpoof] " + ChatColor.GREEN + "Successfully set the playercount to " + args[0] + " Players");
             } catch (NumberFormatException e) {
                 e.printStackTrace();
-                sender.sendMessage(ChatColor.RED + "[BungeePlayerSpoof] " + ChatColor.RED + "You need to type a number");
+                sender.sendMessage(ChatColor.RED + "[BungeePlayerSpoof] " + ChatColor.RED + "You must type a number");
             }
         }
     }
